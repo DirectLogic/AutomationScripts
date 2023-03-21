97e08fe7779a4e8f8c0c60326743f27d3753d3b8
DirectLogicDev <128464655+DirectLogic@users.noreply.github.com>
2023-03-21 10:29:39 +0100

    Update DeleteWindowsFonts.ps1

--- DeleteWindowsFonts.ps1
+++ DeleteWindowsFonts.ps1
@@ -1,6 +1,12 @@
-#The system reserved partition cannot be updated
-#Get partitie with disk type "System"
-Get-Partition | Where-Object -FilterScript {$_.Type -Eq "System"} | Set-Partition -NewDriveLetter o
+
+##Get partition with disk type "System" and set driveletter
+$Partition = Get-Partition | Where-Object -FilterScript {$_.Type -Eq "System"}
+Set-Partition -DiskNumber $Partition.DiskNumber -PartitionNumber $partition.PartitionNumber -NewDriveLetter W 
 
 #Delete all files in folder Fonts.
-Get-ChildItem -File O:\EFI\Microsoft\Boot\Fonts -Recurse | Remove-Item -Recurse
+Get-ChildItem -File W:\EFI\Microsoft\Boot\Fonts -Recurse | Remove-Item -Recurse
+
+#Remove path from system partition
+Remove-PartitionAccessPath -DiskNumber $Partition.DiskNumber -PartitionNumber $partition.PartitionNumber -AccessPath W:
+
+
